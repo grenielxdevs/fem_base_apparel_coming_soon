613d72c946dc24db66814762c7937bbcc370c72d
Greniel Galinato <greniel.galinato@gmail.com>
2024-01-08 20:40:56 +0800

    fem_base_apparel_coming_soon: adding structure and content

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ function App() {
   return (
     <main>
       <section>
-        <div>Logo</div>
-        <div></div>
+        <div>
+          <img src="" alt="" />
+          <h1>Base Apparel</h1>
+        </div>
+        <div>
+          <h2><span>We&apos;re</span>coming soon</h2>
+          <p>Hello fellow shoppers! We&apos;re currently building our new fashion store. 
+Add your email below to stay up-to-date with announcements and our launch deals.</p>
+          <div>
+            <input type="text" placeholder="Email Address"/>
+            <button></button>
+          </div>
+        </div>
       </section>
       <section> 
         <div><img src="" alt="" /></div>
