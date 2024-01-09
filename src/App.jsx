d1681ca760805a0642abbca51302a5e7d478c2ea
Greniel Galinato <greniel.galinato@gmail.com>
2024-01-09 21:28:51 +0800

    fem_base_apparel_coming_soon: adding footer to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <main>
-      <section>
-        <div>
-          <img src="" alt="" />
-          <h1>Base Apparel</h1>
-        </div>
-        <div>
-          <h2><span>We&apos;re</span>coming soon</h2>
-          <p>Hello fellow shoppers! We&apos;re currently building our new fashion store. 
-Add your email below to stay up-to-date with announcements and our launch deals.</p>
+    <>
+      <main>
+        <section>
           <div>
-            <input type="text" placeholder="Email Address"/>
-            <button></button>
+            <img src="" alt="" />
+            <h1>Base Apparel</h1>
           </div>
-        </div>
-      </section>
-      <section> 
-        <div><img src="" alt="" /></div>
-      </section>
-    </main>
+          <div>
+            <h2><span>We&apos;re</span>coming soon</h2>
+            <p>Hello fellow shoppers! We&apos;re currently building our new fashion store. 
+  Add your email below to stay up-to-date with announcements and our launch deals.</p>
+            <div>
+              <input type="text" placeholder="Email Address"/>
+              <button></button>
+            </div>
+          </div>
+        </section>
+        <section> 
+          <div><img src="" alt="" /></div>
+        </section>
+      </main>
+      <Footer />
+    </>
+
   )
 }
 
